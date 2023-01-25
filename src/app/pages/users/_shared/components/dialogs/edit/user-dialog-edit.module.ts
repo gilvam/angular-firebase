ee78d32bfea2a3900889825ff2be349b20972d73
Gilvam Mourão <gilvamm@gmail.com>
2023-01-25 18:04:59 -0300

    fix: cdkFocus on modal

--- src/app/pages/users/_shared/components/dialogs/edit/user-dialog-edit.module.ts
+++ src/app/pages/users/_shared/components/dialogs/edit/user-dialog-edit.module.ts
@@ -4,10 +4,11 @@ import { UserDialogEditComponent } from './user-dialog-edit.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { UserDialogFormModule } from '../_form/user-dialog-form.module';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
 	declarations: [UserDialogEditComponent],
-	imports: [CommonModule, MatDialogModule, MatButtonModule, UserDialogFormModule],
+	imports: [CommonModule, MatDialogModule, MatButtonModule, UserDialogFormModule, A11yModule],
 	exports: [UserDialogEditComponent],
 })
 export class UserDialogEditModule {}
--- src/app/pages/users/_shared/components/dialogs/new/user-dialog-new.module.ts
+++ src/app/pages/users/_shared/components/dialogs/new/user-dialog-new.module.ts
@@ -4,10 +4,11 @@ import { UserDialogNewComponent } from './user-dialog-new.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { UserDialogFormModule } from '../_form/user-dialog-form.module';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
 	declarations: [UserDialogNewComponent],
-	imports: [CommonModule, MatDialogModule, MatButtonModule, UserDialogFormModule],
+	imports: [CommonModule, MatDialogModule, MatButtonModule, UserDialogFormModule, A11yModule],
 	exports: [UserDialogNewComponent],
 })
 export class UserDialogNewModule {}
